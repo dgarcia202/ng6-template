888085549af0ec02f6f5b45afa972d70a555d088
Diego Garcia <wft2000@gmail.com>
2018-10-14 23:16:17 +0200

    exposed list view data source to client component. removed unused code from example template

--- src/app/framework/list-view/list-view.component.ts
+++ src/app/framework/list-view/list-view.component.ts
@@ -1,26 +1,7 @@
 import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { SelectionModel } from '@angular/cdk/collections';
 import {MatTableDataSource} from '@angular/material';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { PublicFeature } from '@angular/core/src/render3';
 
 export interface ListViewColumnDefinition {
   key: string;
@@ -74,20 +55,35 @@ export class ListViewComponent implements OnInit {
 
   displayedColumns: string[] = [ 'position', 'name', 'weight', 'symbol' ];
  
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource = new MatTableDataSource([]);
 
   selection = new SelectionModel(true, []);
 
+  private _data: any[];
+
   constructor() { }
 
+  @Input() set data(value: any[]) {
+    this._data = value;
+    this.dataSource = new MatTableDataSource(this._data);
+  }
+
+  get data(): any[] {
+    return this._data;
+  }
+
   ngOnInit() {
+
+    // Update action buttons enabled state.
     this.selection.changed.subscribe(x => {
       let itemsSelected = x.source.selected.length;
       let editButton: ListViewActionButton = this.defaultActions.filter(y => y.key == 'edit')[0];
       let deleteButton: ListViewActionButton = this.defaultActions.filter(y => y.key == 'delete')[0];
+
       editButton.enabled = itemsSelected == 1;
       deleteButton.enabled = itemsSelected > 0;
     });
+
   }
 
   actionButtons(): ListViewActionButton[] {
